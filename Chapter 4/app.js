c57ac5dc836e34461cd14ae04129ccf7458f0a62
Deepak <0822deepakverma@gmail.com>
2022-05-15 11:59:18 +0530

    union types is a great option

--- Chapter 4/app.js
+++ Chapter 4/app.js
@@ -20,8 +20,12 @@ var person = {
     // this is a ts notation of a specialised object type {}==object
     name: "Deepak Verma",
     age: 30,
-    hobbies: ["Gaming", "Coding", "Reading"]
+    hobbies: ["Gaming", "Coding", "Reading"],
+    role: [2, "Gamer"]
 };
+// person.role.push("Admin"); it is an exception coz ts can't identify this error.
+// person.role[1]=10; this doesn't work coz 10 is a number
+// above in two cases ts didn't verified that the role is of type tuple and it should store only two elements
 var activity;
 // let activity:any[];
 // "any" is a type that can be anything
@@ -34,3 +38,26 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     console.log(hobby.toUpperCase());
 }
 ;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+// UNION TYPES
+// let count:number|boolean;
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combinedAGes = combine(30, 26);
+console.log(combinedAGes);
+var combinedNames = combine("Deepak", "Verma");
+console.log(combinedNames);
--- Chapter 4/app.ts
+++ Chapter 4/app.ts
@@ -50,3 +50,26 @@ for(const hobby of person.hobbies){
 }; 
 
 enum Role {ADMIN, READ_ONLY, AUTHOR};
+
+
+// UNION TYPES
+// let count:number|boolean;
+function combine(input1: number | string, input2: number|string){
+    let result;
+    if(typeof input1==="number" && typeof input2==="number"){
+        result=input1+input2;
+    }else 
+    {
+        result=input1.toString()+input2.toString();
+    }
+
+    return result;
+}
+
+
+const combinedAGes=combine(30,26);
+console.log(combinedAGes);
+
+
+const combinedNames=combine("Deepak   ","Verma");
+console.log(combinedNames);
