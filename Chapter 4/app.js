1a4b8ceaa76cbb95609bc5e261cbb322693746e6
Deepak <0822deepakverma@gmail.com>
2022-05-15 09:41:11 +0530

    string+number= all string

--- Chapter 4/app.js
+++ Chapter 4/app.js
@@ -1,1 +1,13 @@
 console.log("Your code goes here!");
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + n1 + n2);
+        console.log(phrase + result);
+        // if we add a string with numbers, then everything will be concatenated
+    }
+    return n1 + n2;
+}
+var printResult = true;
+var resultPhrase = "Result is: ";
+console.log(add(1, 2.5, printResult, resultPhrase));
--- Chapter 4/app.ts
+++ Chapter 4/app.ts
@@ -1,1 +1,15 @@
 console.log("Your code goes here!");
+
+function add(n1:number, n2:number, showResult:boolean,phrase:string) {
+
+    const result=n1+n2;
+    if (showResult) { 
+        console.log(phrase+n1 + n2);
+        console.log(phrase+result);
+        // if we add a string with numbers, then everything will be concatenated
+     }
+    return n1 + n2;
+}
+const printResult = true;
+const resultPhrase="Result is: ";
+console.log(add(1, 2.5, printResult,resultPhrase));
